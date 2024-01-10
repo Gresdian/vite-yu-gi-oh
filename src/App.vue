9d008e9ff9d988357644f6b75d899ed6e23b5964
Gresdian <chris_da98@outlook.it>
2024-01-10 18:42:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import { store } from './store.js';
+export default {
+
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+<template lang="">
+  <div>
+    Sdasdasd
+  </div>
+</template>
+<style lang="">
+  
+</style>
